39a47c0cc96df680f56c9bca9739585187056b6c
boriy <boriy9144@gmail>
2023-05-02 12:10:55 +0500

    init

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+import {unlink} from 'fs/promises'
+
+export async function removeFile (path) {
+   try{
+     unlink(path)
+   } catch(e){
+     console.log('Error while removing file ', e.message);
+   }
+
+
+}
